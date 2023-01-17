b452f78f5a88ec82a0e58b633c3415cde079bfae
TabethaSnead <tabetha.snead@gmail.com>
2023-01-17 15:13:47 -0800

    wrote displayMushroom function and now you can see the mushrooms in the browser

--- app.js
+++ app.js
@@ -41,35 +41,48 @@ addMushroomButton.addEventListener('click', () => {
 });
 
 addFriendButton.addEventListener('click', () => {
+    console.log([friendData]);
     // get the name from the input
+    const nameInput = friendInputEl.value;
     // create a new friend object
+    const newFriend = { name: nameInput, satisfaction: 1 };
     // push it into the friends state array, passed in as an argument
+    friendData.push(newFriend);
     // clear out the input element
+    friendInputEl.value = '';
     // clear out and display all the friends (use a function here)
+    displayFriends();
 });
 
 function displayFriends() {
     // clear out the friends in DOM
-
+    friendsEl.textContent = '';
     // for each friend in state . . .
     for (let friend of friendData) {
         // use renderFriend to make a friendEl
+        const friendEl = renderFriend(friend);
+        friendsEl.append(friendEl);
 
         // this is a clickable list, so . . .
         //     add an event listener to each friend
+        friendEl.addEventListener('click', () => {
+            alert(friend.name);
+        });
         //         and if the friend's satisfaction level is below 3 and you have mushrooms left
         //             increment the friends satisfaction and decrement your mushrooms
         //             clear out and display the updated friends and mushrooms (hint: displayFriends, displayMushrooms)
-
         // append the friendEl to the friends list in DOM
+        renderFriend(friend);
     }
 }
 
 function displayMushrooms() {
     // clear out the mushroom div
-
+    mushroomsEl.value = '';
     for (let i = 0; i < mushroomCount; i++) {
         // for each mushroom in your mushroom state, render and append a mushroom
+        const displayEl = renderMushroom();
+        mushroomsEl.append(displayEl);
     }
 }
 
